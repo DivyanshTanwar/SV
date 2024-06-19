1b00175b9c826a871e7de52a294690c94cc299e2
Divyansh Tanwar <80058799+DivyanshTanwar@users.noreply.github.com>
2024-06-19 10:13:57 +0530

    Update tbarc_sync_4bitMultiplier_monitor_scoreboard.sv

--- tbarc_sync_4bitMultiplier_monitor_scoreboard.sv
+++ tbarc_sync_4bitMultiplier_monitor_scoreboard.sv
@@ -1,6 +1,18 @@
 //design
 
-
+module top
+(
+  input clk,
+  input [3:0] a,b,
+  output reg [7:0] mul
+);
+  
+  always@(posedge clk)
+    begin
+     mul <= a * b;
+    end
+  
+endmodule
 
 
 //tb
